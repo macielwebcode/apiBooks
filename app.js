613b0ad06e574be7be1e2c0703e1fb05c4f04082
Marcela Maciel <macielwebcode@gmail.com>
2024-02-21 01:22:15 -0300

    ajustes api conectada ao front

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const rotaBook = require("./routers/book")
+const rotaFavorito = require("./routers/favorito")
 const app = express()
+const cors = require("cors")
+
 app.use(express.json()) //aceita receeber requisições json por body
+app.use(cors({
+    origin: "*"
+}))
 
 app.use('/books', rotaBook)
+app.use('/favoritos', rotaFavorito)
 const port = 8000
 
 
---
+++ controllers/favorito.js
@@ -0,0 +1,46 @@
+const { getAllFavoritos, insereFavorito, deletaFavoritoPorId } = require("../services/favoritos")
+
+function getFavoritos(req, res) {
+    try {
+        const livros = getAllFavoritos()
+        res.send(livros)
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    } 
+}
+
+function postFavorito(req, res) {
+    try {
+        const id = req.params.id
+        insereFavorito(id)
+        res.status(201)
+        res.send("Livro inserido com sucesso")
+    } catch(error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function deleteFavorito(req, res) {
+    try {
+        const id = req.params.id
+
+        if(id && Number(id)) {
+            deletaFavoritoPorId(id)
+            res.send("Favorito deletado com sucesso")
+        } else {
+            res.status(422)
+            res.send("ID inválido")
+        }
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    } 
+}
+
+module.exports = {
+    getFavoritos,
+    postFavorito,
+    deleteFavorito
+}
